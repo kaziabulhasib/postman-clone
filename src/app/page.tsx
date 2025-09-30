b2925291dd1a90f82d3ef6cc29a8632e1c5e0a95
kaziabulhasib <hasibkazi420@gmail.com>
2025-10-01 00:35:59 +0530

    authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
-import { Button } from "@/components/ui/button";
+import { currentUser } from "@/modules/authentication/actions";
+import UserButton from "@/modules/components/user-button";
 
+export default async function Home() {
+  
+  const user = await currentUser();
 
-export default function Home() {
   return (
-    <div className='flex h-screen items-center justify-center'>
-      <Button>Click</Button>
+    <div className="flex h-screen items-center justify-center">
+      <UserButton user={user} />
     </div>
   );
 }
